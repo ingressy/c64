357eadb3b9cc69ff4ca1bd95569e12d884080c0f
ingressy <j.czinzoll@ingressy.de>
2025-01-23 11:42:35 +0100

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>  // C64-spezifische Bibliothek für Bildschirmoperationen
+
+void main() {
+    clrscr();  // Bildschirm löschen
+    printf("Hallo User!\n Ich bin die Programmiersprache C");
+    getch();    // Auf eine Tasteneingabe warten
+}
