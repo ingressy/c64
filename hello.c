677bddc91b9b35b52bc17345446da8a6e58964b4
ingressy <j.czinzoll@ingressy.de>
2025-01-23 12:19:35 +0100

    Delete hello.c

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <conio.h>  // C64-spezifische Bibliothek für Bildschirmoperationen
-
-void main() {
-    clrscr();  // Bildschirm löschen
-    printf("Hallo User!\n Ich bin die Programmiersprache C");
-    getch();    // Auf eine Tasteneingabe warten
-}
